0e4085cf5337c9fff6e9b5042073347051d4d141
Uriel Carvajalino <uriel_carva@hotmail.com>
2024-05-29 17:36:09 -0500

    terminacion layout escritorio

---
+++ assets/js/map.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",(event)=>{
+
+    setTimeout(() =>{
+        document.querySelector("#load-iframe-map").innerHTML = `<iframe class="contact__iframe" frameborder="0" scrolling="no" marginheignt="0" marginwidth="0"  loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3948.6746084202555!2d-73.35645252515347!3d8.235438400906078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e677bec71bc08dd%3A0x87799706d32a0a53!2sParque%20Principal%20Oca%C3%B1a!5e0!3m2!1ses!2sco!4v1717015060483!5m2!1ses!2sco"  ></iframe>
+    `
+     ;
+
+    }, 300)
+
+    
+});
